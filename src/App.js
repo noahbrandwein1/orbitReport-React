85ac2572762ee97bbd0c6dbe82e1014c40372b32
noah.brandwein1 <noah.brandwein98@gmail.com>
2023-08-13 15:56:50 -0500

    Updated app.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React, { useState } from 'react';
+import satData from './satData';
 import Banner from "./components/Banner";
 import Buttons from "./components/Buttons";
 import Table from "./components/Table";
@@ -5,11 +7,24 @@ import Table from "./components/Table";
 
 
 function App() {
+  const [sat, setSat] = useState(satData);
+  const displaySats = [...new Set(satData.map((data) => data.orbitType))];
+
+  const filterByType = (currentType) => {
+    const filteredSats = satData.filter((newSatDisplay) => {
+      return newSatDisplay.orbitType === currentType;
+    });
+    setSat(filteredSats);
+  };
+
+
   return (
     <div>
       <Banner />
-      <Buttons />
-      <Table />
+      <Buttons filterByType={filterByType}
+        setSat={setSat}
+        displaySats={displaySats} />
+      <Table sat={sat}/>
     </div>
   );
 }
