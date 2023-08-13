879abdf06ae8700b7cc1aceae9d9c12de148363b
noah.brandwein1 <noah.brandwein98@gmail.com>
2023-08-13 13:41:16 -0500

    banner component.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import Banner from "./components/Banner";
 import Buttons from "./components/Buttons";
 import Table from "./components/Table";
+
 
 
 function App() {
   return (
     <div>
+      <Banner />
       <Buttons />
       <Table />
     </div>
